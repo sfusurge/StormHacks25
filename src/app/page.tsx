2530ed595ea909a1547056a12b2e00a96b8edf84
brendanshen24 <91840687+brendanshen24@users.noreply.github.com>
2025-07-31 21:32:45 -0700

    z level for frame changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
         <div className="font-catriel min-h-screen w-full relative overflow-hidden bg-[#0C0C0B]">
             {/*grain effect*/}
             <Image src="/assets/anim-background.gif" height={10} width={272} alt="horizontal pattern"
-                   className="absolute z-40 w-full h-full object-cover pointer-events-none mix-blend-color-dodge"
+                   className="absolute z-30 w-full h-full object-cover pointer-events-none mix-blend-color-dodge"
             />
             {/*background tiling*/}
             <div className="absolute z-10
@@ -25,18 +25,18 @@ export default function Home() {
                 rotate-45
                 origin-center"
             />
-            <div className="relative z-30 flex flex-row h-screen">
+            <div className="relative flex flex-row h-screen">
                 <Sidebar/>
-                <div className="flex-1 flex flex-col">
-                    <div className="flex justify-between items-start pt-8 px-8">
+                <div className=" flex-1 flex flex-col">
+                    <div className="z-20 flex justify-between items-start pt-8 px-8">
                         <Timer/>
                         <SwapBackground/>
                     </div>
 
-                    <div className="flex-1 flex items-center justify-center">
+                    <div className="z-40 flex-1 flex items-center justify-center">
                         <Frame/>
                     </div>
-                    <div className="flex justify-center pb-4">
+                    <div className="z-20 flex justify-center pb-4">
                         <Controls/>
                     </div>
                 </div>
