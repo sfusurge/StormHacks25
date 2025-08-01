c530f98760e3a3e82da593227ecb86d6cbd5f2d7
brendanshen24 <91840687+brendanshen24@users.noreply.github.com>
2025-07-31 21:42:42 -0700

    background scaling

--- src/components/Frame.tsx
+++ src/components/Frame.tsx
@@ -2,25 +2,27 @@ import Image from "next/image";
 
 export default function Frame() {
     return (
-        <div className="relative w-[872px] h-[511px]">
-            <div className="absolute inset-0 p-[25px]">
-                <div className="relative w-full h-full">
+        <div className="w-full max-w-3/4 aspect-[872/511]">
+            <div className="relative w-full h-full">
+                <div className="absolute inset-0 p-[2.87%]">
+                    <div className="relative w-full h-full">
+                        <Image
+                            src="/backgrounds/background-1.png"
+                            alt="background"
+                            fill
+                            className="object-contain"
+                        />
+                    </div>
+                </div>
+
+                <div className="absolute inset-0 pointer-events-none">
                     <Image
-                        src="/backgrounds/background-1.png"
-                        alt="background"
+                        src="/assets/frame.svg"
+                        alt="frame"
                         fill
                         className="object-contain"
                     />
                 </div>
-            </div>
-
-            <div className="absolute inset-0 pointer-events-none">
-                <Image
-                    src="/assets/frame.svg"
-                    alt="frame"
-                    fill
-                    className="object-contain"
-                />
             </div>
         </div>
     );
